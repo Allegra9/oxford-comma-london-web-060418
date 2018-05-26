a5b0e3a781adf737506eef043bcd982e1e7dc933
Allegra <aukseallegra@gmail.com>
2018-05-26 22:29:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
   last_element = array.pop 
-  puts last_element
   new_arr = array.insert(-1, "and ")
   new_arr = new_arr.join ","
   new_arr += last_element
-  puts new_arr
+  new_arr
 end
