d61584947cee97831951a77a504cdd19a6e6325e
Allegra <aukseallegra@gmail.com>
2018-05-26 22:39:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,16 @@
 def oxford_comma(array)
-  last_element = array.pop 
-  new_arr = array.insert(-1, "and ")
-  new_arr = new_arr.join ", "
-  new_arr += last_element
-  new_arr
+  if array.size == 1
+    str = array.join 
+    puts str
+  elsif array.size == 2
+    str = array.join " and "
+    puts str
+  else  
+    last_element = array.pop 
+    #puts last_element
+    str = array.insert(-1, "and ")
+    str = str.join ", "
+    str += last_element
+    puts str
+  end
 end
